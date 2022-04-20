b7978be6397d874b4f773ab42250342d234d8ab5
Jesse Kovash <jessekovash@gmail.com>
2022-04-20 16:52:30 -0600

    change to reverse string

--- twoPointers/reverseString.js
+++ twoPointers/reverseString.js
@@ -22,16 +22,12 @@
 var reverseString = function(s) {
   let left = 0;
   let right = s.length - 1;
-  while (left !== right) {
+  while (left <= right) {
     let firstValue = s[left];
     s[left] = s[right];
     s[right] = firstValue;
-    if (left + 1 === right) {
-      break;
-    } else {
       left++;
       right--;
-    }
   }
   return s
 };
