45d45af429b2f4ffdba89fb7d43573d3de00f936
Jesse Kovash <jessekovash@gmail.com>
2022-04-20 16:42:01 -0600

    reverse String Complete

---
+++ twoPointers/reverseString.js
@@ -0,0 +1,37 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+var reverseString = function(s) {
+  let left = 0;
+  let right = s.length - 1;
+  while (left !== right) {
+    let firstValue = s[left];
+    s[left] = s[right];
+    s[right] = firstValue;
+    if (left + 1 === right) {
+      break;
+    } else {
+      left++;
+      right--;
+    }
+  }
+  return s
+};
