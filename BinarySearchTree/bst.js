bc01b74291574d7a664237a10556f7a46e362c18
Jesse Kovash <jessekovash@gmail.com>
2022-06-21 14:47:30 -0600

    binary search insert complete

---
+++ BinarySearchTree/bst.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+insert(val) {
+    let newNode = new Node(val)
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currNode = this.root;
+      while(true) {
+        if (val === currNode.value) {
+          return undefined
+        } else if (val < currNode.value) {
+          if (currNode.left) {
+            currNode = currNode.left;
+          } else {
+            currNode.left = newNode;
+          }
+        } else {
+          if (currNode.right) {
+            currNode = currNode.right;
+          } else {
+            currNode.right = newNode
+          }
+        }
+      }
+      val < lastNode.value ? lastNode.left = newNode : lastNode.right = newNode;
+    }
+    return this
+  }
+}
