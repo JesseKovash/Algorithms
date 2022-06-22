98f0dad67474e2dedca205c810d693100a816356
Jesse Kovash <jessekovash@gmail.com>
2022-06-22 11:22:46 -0600

    dfPreOrderSearch complete

---
+++ BinarySearchTree/dfPreOrderSearch.js
@@ -0,0 +1,12 @@
+function dfSearch(root) {
+  let allNodes = [];
+  if (!root) return [];
+  allNodes.push(root.value)
+  if (root.left) {
+    allNodes = allNodes.concat(dfSearch(root.left));
+  }
+  if (root.right) {
+    allNodes = allNodes.concat(dfSearch(root.right));
+  }
+  return allNodes
+}
