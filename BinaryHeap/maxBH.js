ce6a6d0dfa41442269773beebe9f65b4b1268796
Jesse Kovash <jessekovash@gmail.com>
2022-06-23 14:40:24 -0600

    extractmax complete

--- BinaryHeap/maxBH.js
+++ BinaryHeap/maxBH.js
@@ -9,12 +9,13 @@ class MaxBinaryHeap {
 
   insert(value) {
     this.values.push(value);
-    if (this.length === 1) return this;
+    if (this.values.length === 1) return this;
 
     let currIndex = this.values.length - 1;
     let currValue = this.values[currIndex];
     let parentIndex = Math.floor((currIndex - 1) / 2) || 0;
     let parentValue = this.values[parentIndex];
+    //bubble larger values upward
     while (currValue > parentValue) {
       if (parentIndex === 0 && parentValue > currValue) break;
       [this.values[parentIndex], this.values[currIndex]] = [this.values[currIndex], this.values[parentIndex]]
@@ -24,6 +25,42 @@ class MaxBinaryHeap {
       parentValue = this.values[parentIndex]
     }
       return this
+  }
+
+  extractMax() {
+    if (this.values.length === 0) return null;
+    if (this.values.length === 1) this.values = [];
+
+    const max = this.values[0];
+    const end = this.values.pop()
+    this.values[0] = end;
+    let currIndex = 0;
+    let currValue = this.values[0];
+    let rightVal;
+    let leftVal;
+
+    //sink smaller values downwards
+    while (true) {
+      let leftChildIndex = (currIndex * 2) + 1;
+      let rightChildIndex = (currIndex * 2) + 2;
+      let swap = null;
+      leftVal = this.values[leftChildIndex];
+      rightVal = this.values[rightChildIndex];
+
+      if (leftChildIndex < this.values.length) {
+        if (leftVal > currValue) swap = leftChildIndex;
+      }
+
+      if (rightChildIndex < this.values.length) {
+        if (rightVal > currValue && !swap) swap = rightChildIndex;
+        if (rightVal > currValue && rightVal > leftVal) swap = rightChildIndex;
+      }
+
+      if (!swap) break;
+      [this.values[swap], this.values[currIndex]] = [this.values[currIndex], this.values[swap]];
+      currIndex = swap;
+    }
+    return max
   }
 
 }
