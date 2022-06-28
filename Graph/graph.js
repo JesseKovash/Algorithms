489ed1aaa7d4b0bc399dbc54e5ef3c7005ccd206
Jesse Kovash <jessekovash@gmail.com>
2022-06-28 15:54:34 -0600

    graph classes, addedge, removeedge, and dfSearchRecursive complete

---
+++ Graph/graph.js
@@ -0,0 +1,51 @@
+//undirected graph
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1]) this.adjacencyList[vertex1].push(vertex2);
+    if (this.adjacencyList[vertex2]) this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v)=> !==vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex1].filter(
+      (v)=> !==vertex1);
+  }
+
+  removeVertex(vertex) {
+    while(this.adjacencyList[vertex].length) {
+      const adjVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  dfSearchRecursive(vertex) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    function dfs(vert) {
+      if (!vert) return null;
+      if (!visited[vert]) {
+        visited[vert] = true;
+        result.push(vert)
+      }
+      adjacencyList[vert].forEach((one) => {
+        if (!visited[one]) return dfs(one);
+      })
+    }
+    dfs(vertex);
+    return result
+  }
+
+}
